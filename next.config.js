8b19cc78295e7e3cf5a149f67a3e3e2a8d84f462
mzrtamp <mozartrafi@stegripe.org>
2025-04-11 22:47:01 +0700

    chore(nextjs/config): specify standalone output and whitelist more host
    Co-authored-by: noxzym <orchitiadi@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,1 +1,1 @@
-module.exports = { reactStrictMode: true, allowedDevOrigins: ["10.66.99.4"] };
+module.exports = { output: "standalone", reactStrictMode: true, allowedDevOrigins: ["10.66.99.4", "10.66.99.13"] };
